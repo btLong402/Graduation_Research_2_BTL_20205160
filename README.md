8a8cc1862ea2e5716df2f741ce82dac164f40d61
btLong402 <111285054+btLong402@users.noreply.github.com>
2023-09-05 15:06:28 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# drink_shop_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/drink_shop_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drink_shop_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
